4f9252af9452f5b280e30d622a05c2ad7a63a5c9
Shinzen <83638956+Shinzen31@users.noreply.github.com>
2024-04-19 23:52:09 +0200

    Modification
    Nous avons apporté plusieurs modifications clés au fichier CMakeLists.txt et à la fonction mesh_new pour résoudre les problèmes rencontrés lors de la compilation et de l'exécution.
    Modifications du CMakeLists.txt :
    
        Ajout du support MPI : Pour garantir que la bibliothèque MPI soit correctement liée, nous avons ajouté find_package(MPI REQUIRED) dans le CMakeLists.txt. Cela assure que CMake puisse trouver le chemin d'installation de MPI et configure les répertoires d'inclusion et les bibliothèques nécessaires.
        Liaison avec la bibliothèque MPI : Nous avons mis à jour target_link_libraries en ajoutant ${MPI_C_LIBRARIES}, pour garantir que la bibliothèque stencil puisse être liée à la bibliothèque MPI. Cela a résolu le problème de compilation dû à l'absence des fonctions MPI.
        Inclusion des en-têtes MPI : Nous avons ajouté ${MPI_C_INCLUDE_PATH} à target_include_directories pour assurer que le projet puisse trouver les en-têtes MPI.
    
    Ces modifications étaient nécessaires car le projet dépend de MPI pour les calculs parallèles, et la configuration initiale de CMakeLists.txt manquait de ces paramétrages, ce qui empêchait le compilateur de reconnaître le code lié à MPI.
    Modifications des fonctions mesh_new et mesh_drop :
    
        Gestion des erreurs d'allocation mémoire : Dans la fonction mesh_new, nous avons corrigé la logique d'allocation mémoire, en utilisant les bonnes tailles de type de données (sizeof(cell_t*) et sizeof(cell_t**)) pour l'allocation, et nous avons ajouté une gestion des erreurs pour les cas d'échec d'allocation, afin d'éviter les fuites de mémoire.
        Amélioration de la robustesse : En ajoutant une vérification d'erreurs exhaustive et une logique de récupération des ressources en cas d'échec pour chaque dimension d'allocation, nous avons renforcé la robustesse et la fiabilité du programme.
    
    Ces modifications garantissent la sécurité de la gestion de la mémoire lors du traitement de grandes quantités de données, prévenant les crashs du programme ou les comportements imprévisibles dus à une mauvaise gestion de la mémoire. Une allocation de mémoire correcte et une gestion des erreurs sont essentielles pour la stabilité et l'efficacité dans le calcul haute performance

---
+++ TOP-project/src/CMakeFiles/stencil.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/e/vs缓存/TOP/TOP-project/src/stencil/comm_handler.c" "CMakeFiles/stencil.dir/stencil/comm_handler.c.o" "gcc" "CMakeFiles/stencil.dir/stencil/comm_handler.c.o.d"
+  "/mnt/e/vs缓存/TOP/TOP-project/src/stencil/config.c" "CMakeFiles/stencil.dir/stencil/config.c.o" "gcc" "CMakeFiles/stencil.dir/stencil/config.c.o.d"
+  "/mnt/e/vs缓存/TOP/TOP-project/src/stencil/init.c" "CMakeFiles/stencil.dir/stencil/init.c.o" "gcc" "CMakeFiles/stencil.dir/stencil/init.c.o.d"
+  "/mnt/e/vs缓存/TOP/TOP-project/src/stencil/mesh.c" "CMakeFiles/stencil.dir/stencil/mesh.c.o" "gcc" "CMakeFiles/stencil.dir/stencil/mesh.c.o.d"
+  "/mnt/e/vs缓存/TOP/TOP-project/src/stencil/solve.c" "CMakeFiles/stencil.dir/stencil/solve.c.o" "gcc" "CMakeFiles/stencil.dir/stencil/solve.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TOP-project/src/CMakeFiles/stencil.dir/stencil/init.c.o.d
@@ -0,0 +1,36 @@
+CMakeFiles/stencil.dir/stencil/init.c.o: \
+ /mnt/e/vs缓存/TOP/TOP-project/src/stencil/init.c \
+ /usr/include/stdc-predef.h \
+ /mnt/e/vs缓存/TOP/TOP-project/src/../include/stencil/init.h \
+ /mnt/e/vs缓存/TOP/TOP-project/src/../include/stencil/mesh.h \
+ /mnt/e/vs缓存/TOP/TOP-project/src/../include/stencil/../types.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdbool.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/11/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /mnt/e/vs缓存/TOP/TOP-project/src/../include/stencil/comm_handler.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h \
+ /usr/include/assert.h /usr/include/math.h \
+ /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h
